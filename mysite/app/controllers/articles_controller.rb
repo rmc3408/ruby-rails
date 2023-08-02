900288f022d026eae3cb65ed5db6607d3dd51561
rmc3408 <rmc3408@protonmail.com>
2023-08-02 10:56:51 -0400

    articles

---
+++ mysite/app/controllers/articles_controller.rb
@@ -0,0 +1,9 @@
+## Controller
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create; end
+
+end
--- mysite/config/routes.rb
+++ mysite/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#main'
   get '/welcome', to: 'welcome#home'
   get '/welcome/about', to: 'welcome#about'
+
+  resources :articles
 end
